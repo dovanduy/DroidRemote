88c6a57d116431ef75ed5b21fdf66098ef088967
0xFireball <0xFireball@outlook.com>
2016-08-14 22:04:07 +0530

    scan for devices

---
+++ DroidRemote/DroidRemote.Core/States/RemoteViewerState.cs
@@ -0,0 +1,42 @@
+﻿using SharpAdbClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DroidRemote.Core.States
+{
+    public class RemoteViewerState
+    {
+        public DeviceData ConnectedDevice;
+        public string AdbExecutablePath;
+
+        public RemoteViewerState(string adbExecutablePath)
+        {
+            AdbExecutablePath = adbExecutablePath;
+        }
+
+        public async Task PrepareServer()
+        {
+            //Get server ready
+            await Task.Run(() => AdbServer.Instance.StartServer(AdbExecutablePath, true));
+        }
+
+        public bool ConnectToDevice()
+        {
+            var availableDevices = GetAvailableDevices();
+            if (availableDevices.Count == 0)
+            {
+                return false;
+            }
+            ConnectedDevice = availableDevices[0]; //TODO: Use a better method later
+            return true;
+        }
+
+        public List<DeviceData> GetAvailableDevices()
+        {
+            var devices = AdbClient.Instance.GetDevices();
+            //return devices.Select(device=>device.Serial).ToList();
+            return devices;
+        }
+    }
+}
--- DroidRemote/DroidRemote.Windows/VM/RemoteViewerVM.cs
+++ DroidRemote/DroidRemote.Windows/VM/RemoteViewerVM.cs
@@ -1,8 +1,41 @@
-﻿using NanoMvvm;
+﻿using DroidRemote.Core.States;
+using DroidRemote.Windows.Properties;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using NanoMvvm;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace DroidRemote.Windows.VM
 {
     internal class RemoteViewerVM : ViewModelBase
     {
+        public ICommand ViewLoadedCommand => new DelegateCommand(ViewDidLoad);
+
+        private readonly RemoteViewerState _remoteViewerState = new RemoteViewerState(Settings.Default.adbExecutablePath);
+
+        private async void ViewDidLoad(object obj)
+        {
+            await PrepareConnection();
+        }
+
+        private async Task PrepareConnection()
+        {
+            //Get the connection ready
+            var progressController = await (View as MetroWindow).ShowProgressAsync("Connecting", "Establishing a connection");
+            progressController.SetIndeterminate();
+            await _remoteViewerState.PrepareServer();
+            progressController.SetMessage("Preparing device");
+            var connectionStatus = _remoteViewerState.ConnectToDevice();
+            if (!connectionStatus)
+            {
+                //Could not connect to device
+                await progressController.CloseAsync();
+                await (View as MetroWindow).ShowMessageAsync("Error", "The device could not be found");
+                return;
+            }
+            //We're ready!
+            await progressController.CloseAsync();
+        }
     }
 }
