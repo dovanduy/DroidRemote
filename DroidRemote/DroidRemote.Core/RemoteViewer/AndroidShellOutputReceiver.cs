b7a9e096ad04923403a6009208968800c459942b
0xFireball <0xFireball@outlook.com>
2016-08-14 22:21:50 +0530

    add android input command wrapper

---
+++ DroidRemote/DroidRemote.Core/RemoteViewer/AndroidShellOutputReceiver.cs
@@ -0,0 +1,29 @@
+﻿using SharpAdbClient;
+using System.IO;
+
+namespace DroidRemote.Core.RemoteViewer
+{
+    internal class AndroidShellOutputReceiver : IShellOutputReceiver
+    {
+        public Stream ShellOutputStream;
+        public bool ParsesErrors => false;
+
+        private StreamWriter _outputStreamWriter;
+
+        public AndroidShellOutputReceiver()
+        {
+            ShellOutputStream = new MemoryStream();
+            _outputStreamWriter = new StreamWriter(ShellOutputStream) { AutoFlush = true };
+        }
+
+        public void AddOutput(string line)
+        {
+            _outputStreamWriter.WriteLine(line);
+        }
+
+        public void Flush()
+        {
+            _outputStreamWriter.Flush();
+        }
+    }
+}
---
+++ DroidRemote/DroidRemote.Core/RemoteViewer/RemoteAndroidUtils.cs
@@ -0,0 +1,57 @@
+﻿using SharpAdbClient;
+using SharpAdbClient.DeviceCommands;
+
+namespace DroidRemote.Core.RemoteViewer
+{
+    internal class RemoteAndroidUtils
+    {
+        public DeviceData CurrentDevice { get; }
+
+        public RemoteAndroidUtils(DeviceData device)
+        {
+            CurrentDevice = device;
+        }
+
+        public AndroidShellOutputReceiver ExecuteDeviceShellCommand(string command)
+        {
+            var outputReceiver = new AndroidShellOutputReceiver();
+            CurrentDevice.ExecuteShellCommand(command, outputReceiver);
+            return outputReceiver;
+        }
+
+        public void SendInputCommand(string command)
+        {
+            ExecuteDeviceShellCommand($"input {command}");
+        }
+
+        public void PerformTap(double x, double y)
+        {
+            SendInputCommand($"tap {x} {y}");
+        }
+
+        public void PerformSwipe(double x1, double y1, double x2, double y2, long duration)
+        {
+            SendInputCommand($"swipe {x1} {y1} {x2} {y2} {duration}");
+        }
+
+        public void PerformKeypress(int keyCode)
+        {
+            SendInputCommand($"keyevent {keyCode}");
+        }
+
+        public void PerformKeypress(char key)
+        {
+            SendInputCommand($"keyevent {key}");
+        }
+
+        public void PerformKeypress(string keyName)
+        {
+            SendInputCommand($"keyevent {keyName}");
+        }
+
+        public void PerformTextInput(string text)
+        {
+            SendInputCommand($"text {text}");
+        }
+    }
+}
