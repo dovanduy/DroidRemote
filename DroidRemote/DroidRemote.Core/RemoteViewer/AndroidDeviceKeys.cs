f94f8ae6f16ae703bbb289ad150ac6eb8099f27c
0xFireball <0xFireball@outlook.com>
2016-08-15 09:19:24 +0530

    add device key definitions

---
+++ DroidRemote/DroidRemote.Core/RemoteViewer/AndroidDeviceKeys.cs
@@ -0,0 +1,16 @@
+﻿namespace DroidRemote.Core.RemoteViewer
+{
+    public static class AndroidDeviceKeys
+    {
+        public static int KEYCODE_UNKNOWN = 0;
+        public static int KEYCODE_MENU = 1;
+        public static int KEYCODE_SOFT_RIGHT = 2;
+        public static int KEYCODE_HOME = 3;
+        public static int KEYCODE_BACK = 4;
+        public static int KEYCODE_SEARCH = 84;
+        public static int KEYCODE_POWER = 26;
+        public static int KEYCODE_CAMERA = 27;
+        public static int KEYCODE_VOLUME_UP = 24;
+        public static int KEYCODE_VOLUME_DOWN = 25;
+    }
+}
